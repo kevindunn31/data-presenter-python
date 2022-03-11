d5a40fa5cc78e87137fbea45fbe7975f2f0db247
Kevin Dunn <kevindunn31@gmail.com>
2022-03-10 18:01:17 -0600

    data project pythonh

---
+++ data_presenter.py
@@ -0,0 +1,26 @@
+
+data = open('../CupcakeInvoices.csv')
+
+
+for row in data:
+  print(row)
+
+
+for row in data:
+  values = row.split(',')
+  print(values[2])
+
+
+for row in data:
+  values = row.split(',')
+  total = int(values[3]) * float(values[4])
+  print(total)
+
+
+total = 0
+
+for row in data:
+  values = row.split(',')
+  total = total + (int(values[3]) * float(values[4]))
+
+print(total)
